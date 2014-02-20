bf8b9975697a06ddf1799f68793505d785e6146d
wicka <mesaes@gmail.com>
2014-02-20 13:06:01 +0100

    Creacio classe utils

--- Agenda/src/amador/AssigCal.java
+++ Agenda/src/amador/AssigCal.java
@@ -7,23 +7,47 @@ import edu.*;
 
 public class AssigCal {
 	
+	boolean [] dia_mes = new boolean[31];
+	String[][][] cal_pet = new String [2][31][24];
+	
+	private void ini_dia_mes (){
+		for (int i=0; i<dia_mes.length; i++){
+			dia_mes[i] = false;
+		}
+	}
+	
+	private void ini_cal_pet (){
+		for (int i=0; i<cal_pet.length; i++){
+			for (int j=0; j<cal_pet[0].length; j++){
+				for (int k=0; k<cal_pet[0][0].length; k++){
+					cal_pet[i][j][k] = "";
+				}
+			}
+		}
+	}
 
-    public void establirMesAny(Calendar calendario, int mes, int any){
+	public Calendar establirMesAny(Calendar calendario, int mes, int any){
         calendario.set(Calendar.YEAR,any);
         calendario.set(Calendar.MONTH,mes-1);
         calendario.set(Calendar.DAY_OF_MONTH,1);
+        return calendario;
     }
     
-    public void establirDia(Calendar calendario, int dia){
+    public Calendar establirDia(Calendar calendario, int dia){
         calendario.set(Calendar.DAY_OF_MONTH,dia);
+        System.out.println("Dia mes: " + calendario.get(Calendar.DAY_OF_MONTH) + "Dia setmana: " + calendario.get(Calendar.DAY_OF_WEEK));
+        return calendario;
     }
     
     public void assignarDiaMes(Calendar calendario){
 		Entrada ent = new Entrada();
 
     	String [] dates = new String[3];
-    	boolean [] dia_mes = new boolean[31];
-
+    	ini_dia_mes();
+    	ini_cal_pet();
+    	
+    	
+    
 		for (int i=0; i<ent.peticions.length; i++){
 
 			// Obtenim el dia inici i final del rang de dates
@@ -36,8 +60,10 @@ public class AssigCal {
 	    	for (int j=0; j<ent.dies.length; j++){
 	    		// Obtenim els dies de la setmana
 	    		if (ent.dies[i][j] == true){
-	    			cercaDiaSetmana(calendario, dia_mes, j+1, dia_ini, dia_fi);
+	    			cercaDiaSetmana(calendario, j+1, dia_ini, dia_fi);
 	    		}
+	    		
+	    		
 			}
 
 	    	System.out.println( Arrays.toString(dia_mes));
@@ -45,11 +71,12 @@ public class AssigCal {
     	
     }
     
-    private void cercaDiaSetmana(Calendar calendario, boolean[] dia_mes, int dia_set,
+    private void cercaDiaSetmana(Calendar calendario, int dia_set,
     		int dia_ini, int dia_fi){
     	for (int i=dia_ini; i <= dia_fi; i++){
-    		establirDia(calendario, i);
-    		if (calendario.DAY_OF_WEEK == dia_set){
+    		calendario = establirDia(calendario, i);
+            System.out.println("Dia set1: " + dia_set + " Dia setmana: " + calendario.get(Calendar.DAY_OF_WEEK));
+    		if (calendario.get(Calendar.DAY_OF_WEEK) == dia_set){
     			dia_mes[i] = true;
     		}
     	}
--- Agenda/src/amador/MainAmador.java
+++ Agenda/src/amador/MainAmador.java
@@ -2,19 +2,38 @@ package amador;
 
 //import java.util.Arrays;
 
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Locale;
 
 import edu.Configuracion;
+import ferran.Principal;
+import ferran.Utiles;
 import amador.Entrada;
 
 public class MainAmador {
 
 	public static void main(String[] args) {
 
-		AssigCal cal = new AssigCal();
-		Calendar calendario = Calendar.getInstance();
 		Configuracion conf = new Configuracion("config.txt");
+		Principal princ = new Principal();
 		
+		
+		String[][] peticionsOrdenades = Utiles.sortRequest();
+
+		for (int i = 0; i < 16; i++) {
+			System.out.println(Arrays.toString(peticionsOrdenades[i]));
+		}
+		
+		
+		Calendar calendario = Calendar.getInstance(Locale.FRANCE);
+		
+		AssigCal cal = new AssigCal();
+
+		int mes = conf.getMesConf();
+		int any = conf.getAnyConf();
+
+		calendario = cal.establirMesAny(calendario, mes, any);
 		cal.assignarDiaMes(calendario);
     	
 		for (int i=0; i<Entrada.peticions.length; i++){
@@ -24,9 +43,6 @@ public class MainAmador {
 			System.out.println();
 		}
 		
-		int mes = conf.getMesConf();
-		int any = conf.getAnyConf();
-
 		System.out.println("Any: " + calendario.get(Calendar.YEAR) + "; Mes:  " + (calendario.get(Calendar.MONTH)+1));
 		cal.establirMesAny(calendario, mes, any);
 		System.out.println("Any: " + calendario.get(Calendar.YEAR) + "; Mes:  " + (calendario.get(Calendar.MONTH)+1));

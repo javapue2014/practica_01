894900b6868bf38b49da8bf944563d859e7fbcc4
Ferran <ferranpar@yahoo.com>
2014-02-26 12:43:59 +0100

    Canvi getNumSemanas()

--- Agenda/src/ferran/Calendario_ayuda.java
+++ Agenda/src/ferran/Calendario_ayuda.java
@@ -3,26 +3,29 @@ package ferran;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-import edu.Configuracion;
-import edu.Prioridades;
-
 public class Calendario_ayuda {
+	private static int numSemanas;
+
+	public static int getnumSemanas() {
+		return numSemanas;
+	}
 
 	public static void main(String[] args) {
-		Configuracion conf = new Configuracion("config.txt");
-		Prioridades prio = new Prioridades(conf);
 
 		ArrayList<boolean[]> calen = Calendario.trueOFalse();
 
-		for (int i = 0; i < calen.size(); i++) {
-			System.out.println(Arrays.toString(calen.get(0)));
-
-		}
+//		for (int i = 0; i < calen.size(); i++) {
+//			System.out.println(Arrays.toString(calen.get(0)));
+//
+//		}
 
 		// semana del año
-		System.out.println(Calendario.getSemana1());
+		//System.out.println("Semana del año :" + Calendario.getSemana1());
 		// semanas que tiene el mes
-		System.out.println(calen.size());
+		//System.out.println(calen.size());
+		
+		numSemanas = calen.size();
+		
 
 	}
 }
